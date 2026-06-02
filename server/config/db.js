bcc3aa23d2ceed2fbb022ec66f6b3e1438a16a88
Thanzia Patel Raheem <thanzia.pr@gmail.com>
2026-06-02 20:28:45 +0800

    Add Docker and Docker Compose setup

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (req, res) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
